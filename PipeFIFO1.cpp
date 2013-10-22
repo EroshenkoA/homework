7322a9adccf652ffb8b4ea9d9215c29824bea283
EroshenkoA <furysham@mail.ru>
2013-10-22 19:15:47 +0400

    better style

--- PipeFIFO1.cpp
+++ PipeFIFO1.cpp
@@ -8,78 +8,80 @@
 #include <errno.h>
 #include <sys/stat.h>//for umask
 #include <stdlib.h>//for exit
-int main (int argc, char** argv){
-	int pip1[2];
-	if (pipe(pip1)<0) {printf("can't create pipe1\n"); exit(-1);}
-	close(1); //close standart output, now 1 is free
-	(void) umask(0); //needed  to create file with appropriate mask
-	int fd;//it is going to be our new output stream, because will receive number 1 
-	//fd=open("minifile",O_RDWR|O_CREAT,0666);
-	//fd=open("minifile",O_RDONLY);	
-	fd=open("minifile",O_WRONLY|O_CREAT,0666);
-	int son;
-	if (son=fork())
-		{//case parent
-			//close(pip1[0]);//will write only
-			int son3;	
-					if ((son3=fork())!=0)
-						{//case parent
-						//printf("here just for fun\n");						
-							execl("/bin/ls","ls",argv[1],NULL);//call ls, result will be in file
-						}
-					else
-						{//case second son					
-							int sizep;
-							int inpipesize;
-							char string[100];
-							int i;
-							pid_t ppid;
-							while ((ppid=getppid())!=1) {ppid=0;}//guarantees that file exists and full
-							close(fd);
-							fd=open("minifile",O_RDONLY);
-							while ( (sizep=read(fd,string,100))>0)
-								{
-									printf("sizep %d \n",sizep);
-									//assume blocking is on
-									i=0;
-								while ((sizep>0)&&(inpipesize=write(pip1[1],string+i,sizep)<sizep))
-										{//unsuccesfull writing
-								//if (inpipesize<0) {printf("can't inpipe\n"); exit(-1);}//no blocking
-											i+=inpipesize;
-											sizep-=inpipesize;
-										}
-									
-								}
-							//now pipe work is done
-							close(fd);
-							close(pip1[1]);
-							execl("/bin/rm","rm","minifile",NULL);
-						}
-		}		
-	else	//case first son
-		{
-			close(pip1[1]);//will read only
-			char string[101];
-			string[100]='\0';
-			int size;
-			(void)umask(0);
-			mknod("minififo",S_IFIFO|0666,0);//creates fifo
-			int fifofd=open("minififo",O_WRONLY);//without O_NDELAY flag it is blocked until someone reads it
-			int infifo;
-			int i=0;
-			while ((size=read(pip1[0],string,100))>0)
-				{
-					string[size]='\0';
-					infifo=write(fifofd,string,size);
-					i=infifo;
-					while ((infifo>0)&&(i<size))
-						{
-							infifo=write(fifofd,string+i,size);
-							i+=infifo;
-						}
-				}
-			close(pip1[0]);
-			close(fifofd);
+#define N 100 //buffer capacity, copying from file to pipe[] through string[N]
+#define M 100 //string length, copying from pipe[] to fifo via string[M]
+int main (int argc, char** argv)
+{
+int pipefd[2];
+if (pipe(pipefd)<0) 
+{
+	printf("can't create pipe1\n");
+	 exit(-1);
+}
+close(1); //close standart output, now 1 is free
+(void) umask(0); //needed  to create file with appropriate mask
+int fd;//it is going to be our new output stream, because will receive number 1 	
+fd=open("minifile",O_WRONLY|O_CREAT,0666);
+int son;
+if (son=fork())
+	{//case parent
+		close(pipefd[0]);//will write only
+		int son2;	
+		if ((son2=fork())!=0)
+		{//case parent						
+			execl("/bin/ls","ls",argv[1],NULL);//call ls, result will be in file
 		}
+		else
+		{//case second son					
+			int sizep;
+			int inpipesize;
+			char string[N];
+			int i;
+			pid_t ppid;
+			while ((ppid=getppid())!=1) //do nothing until parent has finished his execl
+				{
+					ppid=0;
+				}//this cycle guarantees that file exists and full
+			close(fd); //now need to read the file, not to write; cursor position must be changed  
+			fd=open("minifile",O_RDONLY);
+			while ( (sizep=read(fd,string,N))>0)
+				{
+					//printf("sizep %d \n",sizep);
+					i=0;
+					while ((sizep>0)&&(inpipesize=write(pipefd[1],string+i,sizep)<sizep))
+						{//case of unsuccesfull writing
+							i+=inpipesize;
+							sizep-=inpipesize;
+						}			
+				}
+			//now pipe work is done
+			close(fd);
+			close(pipefd[1]);
+			execl("/bin/rm","rm","minifile",NULL);
+		}
+	}		
+else	//case first son
+	{
+		close(pipefd[1]);//will read only
+		char string[M];
+		int size;
+		(void)umask(0);
+		mknod("minififo",S_IFIFO|0666,0);//creates fifo
+		int fifofd=open("minififo",O_WRONLY);//without O_NDELAY flag it is blocked until someone reads it
+		int infifo;
+		int i=0;
+		while ((size=read(pipefd[0],string,M))>0)
+			{
+				infifo=write(fifofd,string,size);
+				i=infifo;
+				while ((infifo>0)&&(i<size))
+					{
+						infifo=write(fifofd,string+i,size);
+						i+=infifo;
+					}
+			}
+		close(pipefd[0]);
+		close(fifofd);
+	}
 return 0;	//politeness
 }
--- PipeFIFO2.cpp
+++ PipeFIFO2.cpp
@@ -7,29 +7,35 @@
 #include <malloc.h> //to play with string
 #include <errno.h>
 #include <sys/stat.h>//for umask
-#include <stdlib.h>//for exit 
-int main (){
-	int fd;
-	if ((fd=open("minififo",O_RDONLY|O_NDELAY))<0)
-		{printf("can't open fifo for reading\n"); exit(-1);}
-	char s[100];
-	int size;
-	int j=0;
-	while ((size=read(fd,s,100))!=0)//I know I should have used define
+#include <stdlib.h>//for exit
+#define N 100 //buffer length 
+int main ()
+{
+int fd;
+if ((fd=open("minififo",O_RDONLY|O_NDELAY))<0)
+	{
+		printf("can't open fifo for reading\n"); 
+		exit(-1);
+	}
+char s[N];
+int size;
+int j=0; //-1 branch counter
+while ((size=read(fd,s,N))!=0)
+	{
+		if (size>0)
 		{
-			if (size>0)
-			{
 			s[size]='\0';
 			printf("%s",s);
-			}
-			else
-				{
-					j++;
-					/*if (j==10000000)
-					{break;}*/
-				}
 		}
-	close(fd);
-	execl("/bin/rm","rm","minififo",NULL);
+		else
+		{
+			j++;
+			/*if (j==10000000)
+			{break;}*/
+			//printf("j=%d\n",j);
+		}
+	}
+close(fd);
+execl("/bin/rm","rm","minififo",NULL);
 return 0;	//politeness
 }
